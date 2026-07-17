2c8944bf461ff0fa9cf3e8593930b6dd73d40f62
RomesAll <romesky845@gmail.com>
2026-07-17 03:46:31 +0300

    chore: обновил пакеты

---
+++ src/business/__init__.py
@@ -0,0 +1,10 @@
+from .backup_creator import BackUpCreator
+from .hash_adapter import Sha256Hash
+
+__version__ = 'v1.0.0'
+__author__ = 'Romesky'
+
+__all__ = [
+    'BackUpCreator',
+    'Sha256Hash'
+]
---
+++ src/presentation/__init__.py
@@ -0,0 +1,8 @@
+from .cli.client import CliClient
+
+__version__ = 'v1.0.0'
+__author__ = 'Romesky'
+
+__all__ = [
+    'CliClient'
+]
