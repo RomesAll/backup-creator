505faf90092d2e167c7ba0e8337fb603b7ca280b
RomesAll <romesky845@gmail.com>
2026-07-17 10:14:49 +0300

    refator: убрал лишний print

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import fire, sys
 sys.path.insert(0, (str(Path(__file__).parent.parent.resolve())))
-print(sys.path)
 from src.presentation.cli.client import CliClient
 
 if __name__ == '__main__':
