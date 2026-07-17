d25e47bf3881ef5518a9c25e449ad9d421d041f5
RomesAll <romesky845@gmail.com>
2026-07-17 12:47:22 +0300

    chore: обновить версии пакетов

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,7 +2,7 @@ from .database import mongo_manager
 from .models import DirMetaInfo, FileMetaInfo, MetaInfo, MetaInfoGet, SourcePath
 from .repositories import IRepository, MongoAdapter
 
-__version__ = 'v1.0.0'
+__version__ = 'v1.1.0'
 __author__ = 'Romesky'
 
 __all__ = [
