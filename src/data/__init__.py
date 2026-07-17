231d7b4df24aa4127598fd01417d7e889a3dec32
RomesAll <romesky845@gmail.com>
2026-07-17 10:10:43 +0300

    fix: исправил иморты

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,4 @@
-from .database import client, database
+from .database import mongo_manager
 from .models import DirMetaInfo, FileMetaInfo, MetaInfo, MetaInfoGet, SourcePath
 from .repositories import IRepository, MongoAdapter
 
@@ -6,8 +6,7 @@ __version__ = 'v1.0.0'
 __author__ = 'Romesky'
 
 __all__ = [
-    'client',
-    'database',
+    'mongo_manager',
     'DirMetaInfo',
     'FileMetaInfo',
     'MetaInfo',
