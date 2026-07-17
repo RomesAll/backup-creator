d694697f770a750f73ecedae54f22d2c02efe535
RomesAll <romesky845@gmail.com>
2026-07-17 06:45:22 +0300

    feat: добавил обработку ошибок в репозитории mongodb адаптере

--- src/data/repositories.py
+++ src/data/repositories.py
@@ -1,19 +1,29 @@
 from abc import ABC, abstractmethod
 from pymongo.database import Database
+from pymongo.errors import ServerSelectionTimeoutError, ConnectionFailure, OperationFailure, InvalidName
+from pymongo.results import UpdateResult
+
+from src.data.exceptions import MetaInfoNotFound, GetMetaInfoNotFound, UpdateMetaInfo, ServerIsNotRunning, AuthError, \
+    InCorrectNameDb, ConnectionError
 from src.data.models import DirMetaInfo, FileMetaInfo, MetaInfo, MetaInfoGet
-from hashlib import sha256
 from functools import wraps
 
 def set_collections(func):
     @wraps(func)
     def wrapper(self, meta: MetaInfoGet):
-        # self.source_hash = sha256(
-        #     str(meta.source.resolve()).encode()
-        # ).hexdigest()
-        self.collection = self.database[
-            str(meta.source.resolve())
-        ]
-        return func(self, meta)
+        try:
+            self.collection = self.database[
+                str(meta.source.resolve())
+            ]
+            return func(self, meta)
+        except ServerSelectionTimeoutError as e:
+            raise ServerIsNotRunning(f'Сервер не запущен или недоступен')
+        except ConnectionFailure as e:
+            raise ConnectionError(f'Не удалось подключиться к серверу, неверный порт, фаервол')
+        except OperationFailure as e:
+            raise AuthError(f'Ошибка аунтификации, подробнее: {e}')
+        except InvalidName as e:
+            raise InCorrectNameDb(f'Некорректное имя бд, подробнее: {e}')
     return wrapper
 
 class IRepository(ABC):
@@ -28,7 +38,7 @@ class IRepository(ABC):
 class MongoAdapter(IRepository):
     def __init__(self, database: Database):
         self.database = database
-        self.collection = database['base_collection']
+        self.collection = None
 
     @set_collections
     def get_metadata(self, meta: MetaInfoGet):
@@ -36,15 +46,22 @@ class MongoAdapter(IRepository):
             meta.target_path.is_file: FileMetaInfo,
             meta.target_path.is_dir: DirMetaInfo
         }
-        metadata: dict | None = self.collection.find_one({
+        find_filter: dict = {
             'target_path': str(meta.target_path.resolve()),
-        }, {'_id': 0})
+        }
+        metadata: dict | None = self.collection.find_one(
+            find_filter,
+            {'_id': 0}
+        )
         if not metadata:
-            return None
+            raise MetaInfoNotFound(find_filter)
         for func, dto_type in mapping.items():
             if metadata and func():
                 return dto_type(**metadata)
-        return metadata
+        raise GetMetaInfoNotFound(
+            str(meta.target_path.resolve()),
+            metadata
+        )
 
     @set_collections
     def save_metadata(self, meta: MetaInfo):
@@ -66,9 +83,11 @@ class MongoAdapter(IRepository):
         }
         if meta.target_path.is_file():
             update_data['$setOnInsert']['hash'] = meta.hash
-        result = self.collection.update_one(
+        result: UpdateResult = self.collection.update_one(
             filter_query,
             update_data,
             upsert=True
         )
+        if result.matched_count != 1:
+            raise UpdateMetaInfo(update_data, result.matched_count)
         return result
