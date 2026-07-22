04f392662186a6beb3f2ea237682ebee20088568
RomesAll <romesky845@gmail.com>
2026-07-22 17:24:56 +0300

    refactor: добавил вывод pid процесса в логах

--- src/logging_conf.py
+++ src/logging_conf.py
@@ -1,11 +1,11 @@
 import logging
-from src.config import config
+from src.config import config, BASE_DIR
 
 logger = logging.getLogger(config.logging.name_app_logger)
 logger.setLevel(config.logging.level)
 
 formatter = logging.Formatter(
-    fmt="%(asctime)s [%(levelname)s] %(module)s %(name)s: %(message)s",
+    fmt="%(asctime)s [PID: %(process)d] [%(levelname)s] %(module)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
@@ -13,9 +13,10 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(config.logging.level)
 console_handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler('app.log')
+file_handler = logging.FileHandler(f'{BASE_DIR}/app.log')
 file_handler.setLevel('INFO')
 file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+logger.propagate = False
